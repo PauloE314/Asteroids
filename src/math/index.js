f05e8a44666a78d751c0685b37a7fd206ae9dbe7
Paulo <pauloeduardodelima155@gmail.com>
2021-03-31 13:16:29 -0300

    Deg to rad converter created

--- src/math/index.js
+++ src/math/index.js
@@ -1,3 +1,15 @@
+/**
+ * @param {Number} a
+ * @param {Number} b
+ * @returns {Number}
+ */
 export function getSmaller(a, b) {
     return a < b ? a : b;
+}
+
+/**
+ * @param {Number} n
+ */
+export function radian(n) {
+    return (Math.PI / 180) * n;
 }
